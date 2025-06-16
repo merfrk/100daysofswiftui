324537793431bbf208d89362475f447e60099ebc
Ömer Faruk ARTAN <o.farukartan@hotmail.com>
2025-06-16 23:16:19 +0300

    First release of Word Scramble

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Omer on 15.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
