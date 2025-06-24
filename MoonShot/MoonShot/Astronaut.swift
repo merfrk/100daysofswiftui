12700451d49500801b3a82a3e7637b9323751c70
merfrk <o.farukartan@hotmail.com>
2025-06-24 19:41:58 +0300

    add moonshot app

---
+++ MoonShot/MoonShot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Omer on 24.06.2025.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable{
+    let id: String
+    let name: String
+    let description: String
+}
+
---
+++ MoonShot/MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Omer on 24.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
