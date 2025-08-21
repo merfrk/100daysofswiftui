a5ebfba64dab9648bead08facbbe37a0356eae55
merfrk <o.farukartan@hotmail.com>
2025-08-21 20:40:25 +0300

    add cupcakecornerr

---
+++ CupcakeCornerr/CupcakeCornerr/CupcakeCornerrApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerrApp.swift
+//  CupcakeCornerr
+//
+//  Created by Omer on 19.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
