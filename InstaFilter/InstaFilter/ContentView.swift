0d4ff1a9ce7b09eefee19ef118631f3af4fce19f
merfrk <o.farukartan@hotmail.com>
2025-08-25 23:25:40 +0300

    feat: add confirmationDialog

--- InstaFilter/InstaFilter/ContentView.swift
+++ InstaFilter/InstaFilter/ContentView.swift
@@ -8,21 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount = 0.0{
-        didSet {
-                print("New value is \(blurAmount)")
-            }
-    }
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
     var body: some View {
-        VStack {
-                    Text("Hello, World!")
-                        .blur(radius: blurAmount)
-
-                    Slider(value: $blurAmount, in: 0...20)
-
-                    Button("Random Blur") {
-                        blurAmount = Double.random(in: 0...20)
-                    }
+        Button("Hello, World!") {
+                    showingConfirmation = true
+                }
+                .frame(width: 300, height: 300)
+                .background(backgroundColor)
+                .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+                    Button("Red") { backgroundColor = .red }
+                    Button("Green") { backgroundColor = .green }
+                    Button("Blue") { backgroundColor = .blue }
+                    Button("Cancel", role: .cancel) { }
+                } message: {
+                    Text("Select a new color")
                 }
     }
 }
