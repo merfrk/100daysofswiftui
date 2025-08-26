7552524e771d1eb73bbf27137350badddacf9da9
merfrk <o.farukartan@hotmail.com>
2025-08-26 17:59:44 +0300

    feat: add PhotosPicker

--- InstaFilter/InstaFilter/ContentView.swift
+++ InstaFilter/InstaFilter/ContentView.swift
@@ -4,26 +4,56 @@
 //
 //  Created by Omer on 25.08.2025.
 //
-
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
+    
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    @State private var selectedItem: PhotosPickerItem?
+    
     var body: some View {
-        Button("Hello, World!") {
-                    showingConfirmation = true
+        NavigationStack{
+            VStack{
+                Spacer()
+                PhotosPicker(selection: $selectedItem){
+                    if let processedImage{
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else{
+                        ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to Import a Photo"))
+                    }
                 }
-                .frame(width: 300, height: 300)
-                .background(backgroundColor)
-                .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-                    Button("Red") { backgroundColor = .red }
-                    Button("Green") { backgroundColor = .green }
-                    Button("Blue") { backgroundColor = .blue }
-                    Button("Cancel", role: .cancel) { }
-                } message: {
-                    Text("Select a new color")
+                .buttonStyle(.plain)
+                .onChange(of: selectedItem, loadImage)
+                Spacer()
+                
+                HStack{
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
                 }
+                .padding(.vertical)
+                
+                HStack{
+                    Button("Change Filter", action: changeFilter)
+                    Spacer()
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("InstaFilter")
+        }
+    }
+    func changeFilter(){
+        
+    }
+    func loadImage(){
+        Task{
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return}
+            
+        }
     }
 }
 
