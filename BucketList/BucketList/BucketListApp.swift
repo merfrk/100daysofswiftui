10dd2ab0405f2ca8fa4cc9e10f5da2f6662c1b94
merfrk <o.farukartan@hotmail.com>
2025-08-27 23:43:31 +0300

    chore: initial setup for BucketList app

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Omer on 27.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
