a4a275f7d58e04cfd534a770260e8fe6dfd823f4
merfrk <o.farukartan@hotmail.com>
2025-08-28 16:03:32 +0300

    feat: add FaceID

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -4,72 +4,40 @@
 //
 //  Created by Omer on 27.08.2025.
 //
+import LocalAuthentication
 import MapKit
 import SwiftUI
 
 struct ContentView: View {
-    struct Location: Identifiable {
-        let id = UUID()
-        var name: String
-        var coordinate: CLLocationCoordinate2D
-    }
-    
-    @State private var position = MapCameraPosition.region(
-        MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
-            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-        )
-        )
-    let locations = [
-        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-    ]
+    @State private var isUnlocked = false
     
     var body: some View {
-        VStack{
-            MapReader { proxy in
-                Map(position: $position){
-                    ForEach(locations) { location in
-                        Annotation(location.name, coordinate: location.coordinate) {
-                            Text(location.name)
-                                .font(.headline)
-                                .padding()
-                                .background(.blue)
-                                .foregroundStyle(.white)
-                                .clipShape(.capsule)
-                        }
-                        .annotationTitles(.hidden)
-                    }
-                }
-                .onTapGesture { position in
-                           if let coordinate = proxy.convert(position, from: .local) {
-                               print(coordinate)
-                           }
-                       }
+        VStack {
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
             }
-                .mapStyle(.hybrid(elevation: .realistic))
-            
-                .onMapCameraChange { context in
-                        print(context.region)
-                    }
-            
-            Button("Paris") {
-                    position = MapCameraPosition.region(
-                        MKCoordinateRegion(
-                            center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
-                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                        )
-                    )
-                }
+        }
+        .onAppear(perform: authenticate)
 
-                Button("Tokyo") {
-                    position = MapCameraPosition.region(
-                        MKCoordinateRegion(
-                            center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
-                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                        )
-                    )
+    }
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            let reason = "We need to unlock your data"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason){ success, authanticationError in
+                if success{
+                    isUnlocked = true
+                }else{
+                    
                 }
+            }
+        } else{
+            // no biometrics
         }
     }
 }
