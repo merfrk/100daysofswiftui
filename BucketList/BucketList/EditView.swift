fb6d7e8456f72879cc7dd4a0ab0eff8b4c55a0a6
merfrk <o.farukartan@hotmail.com>
2025-08-29 23:44:44 +0300

    feat: integrate wikipedia api

--- BucketList/BucketList/EditView.swift
+++ BucketList/BucketList/EditView.swift
@@ -13,6 +13,8 @@ struct EditView: View {
     @State private var name: String
     @State private var description: String
     var onSave: (Location) -> Void
+    @State private var loadingState = LoadingState.loading
+    @State private var pages = [Page]()
     
     var body: some View {
         NavigationStack{
@@ -20,6 +22,22 @@ struct EditView: View {
                 Section{
                     TextField("Place name", text: $name)
                     TextField("Description", text: $description)
+                }
+                Section("Nearby…") {
+                    switch loadingState {
+                    case .loaded:
+                        ForEach(pages, id: \.pageid) { page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(": ") +
+                            Text(page.description)
+                                .italic()
+                        }
+                    case .loading:
+                        Text("Loading…")
+                    case .failed:
+                        Text("Please try again later.")
+                    }
                 }
             }
             .navigationTitle("Place Details")
@@ -34,6 +52,31 @@ struct EditView: View {
                     dismiss()
                 }
             }
+            .task{
+                await fetchNearbyPlaces()
+            }
+        }
+    }
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+
+            // we got some data back!
+            let items = try JSONDecoder().decode(Result.self, from: data)
+
+            // success – convert the array values to our pages array
+            pages = items.query.pages.values.sorted()
+            loadingState = .loaded
+        } catch {
+            // if we're still here it means the request failed somehow
+            loadingState = .failed
         }
     }
     init(location: Location, onSave: @escaping (Location) -> Void) {
@@ -42,6 +85,9 @@ struct EditView: View {
         
         _name = State(initialValue: location.name)
         _description = State(initialValue: location.description)
+    }
+    enum LoadingState{
+        case loading, loaded, failed
     }
 }
 
