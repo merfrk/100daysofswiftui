c76873b7e2d56e6e0aa8fd3e54e5198861cc8852
merfrk <o.farukartan@hotmail.com>
2025-08-29 14:09:11 +0300

    feat: add Annotations

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -9,37 +9,42 @@ import MapKit
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isUnlocked = false
+    @State private var locations = [Location]()
+    @State private var selectedPlace: Location?
     
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 56, longitude: -3),
+            span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+        )
+    )
     var body: some View {
-        VStack {
-            if isUnlocked {
-                Text("Unlocked")
-            } else {
-                Text("Locked")
+        MapReader{ proxy in
+            Map(initialPosition: startPosition){
+                ForEach(locations) { location in
+                    Annotation(location.name, coordinate: location.coordinate) {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundStyle(.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(.circle)
+                    }
+                    }
             }
+                .onTapGesture { position in
+                    if let coordinate = proxy.convert(position, from: .local){
+                        let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
+                            locations.append(newLocation)
+                    }
+                }
+                .sheet(item: $selectedPlace){ place in
+                    Text(place.name)
+                }
         }
-        .onAppear(perform: authenticate)
 
     }
-    func authenticate() {
-        let context = LAContext()
-        var error: NSError?
-        
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
-            let reason = "We need to unlock your data"
-            
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason){ success, authanticationError in
-                if success{
-                    isUnlocked = true
-                }else{
-                    
-                }
-            }
-        } else{
-            // no biometrics
-        }
-    }
+    
 }
 
 #Preview {
