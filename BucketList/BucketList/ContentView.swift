318cc1ebe74b122280cd62a5f95c43819f5f0808
merfrk <o.farukartan@hotmail.com>
2025-08-29 21:43:41 +0300

    feat: add EditView

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -23,26 +23,35 @@ struct ContentView: View {
             Map(initialPosition: startPosition){
                 ForEach(locations) { location in
                     Annotation(location.name, coordinate: location.coordinate) {
-                        Image(systemName: "star.circle")
-                            .resizable()
-                            .foregroundStyle(.red)
-                            .frame(width: 44, height: 44)
-                            .background(.white)
-                            .clipShape(.circle)
+                        Button {
+                            selectedPlace = location
+                        } label: {
+                            Image(systemName: "star.circle")
+                                .resizable()
+                                .foregroundStyle(.red)
+                                .frame(width: 44, height: 44)
+                                .background(.white)
+                                .clipShape(Circle())
+                        }
+                        .buttonStyle(PlainButtonStyle())
                     }
-                    }
+                }
             }
-                .onTapGesture { position in
-                    if let coordinate = proxy.convert(position, from: .local){
-                        let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
-                            locations.append(newLocation)
+            .onTapGesture { position in
+                if let coordinate = proxy.convert(position, from: .local){
+                    let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
+                    locations.append(newLocation)
+                }
+            }
+            .sheet(item: $selectedPlace){ place in
+                EditView(location: place) { newLocation in
+                    if let index = locations.firstIndex(of: place) {
+                        locations[index] = newLocation
                     }
                 }
-                .sheet(item: $selectedPlace){ place in
-                    Text(place.name)
-                }
+            }
         }
-
+        
     }
     
 }
