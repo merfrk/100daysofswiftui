00f6b36b26712dbc42930c44f3ff502474b50617
merfrk <o.farukartan@hotmail.com>
2025-09-01 23:43:33 +0300

    feat: create PhotoLabel project

---
+++ PhotoLabel/PhotoLabel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoLabel
+//
+//  Created by Omer on 1.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoLabel/PhotoLabel/PhotoLabelApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoLabelApp.swift
+//  PhotoLabel
+//
+//  Created by Omer on 1.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct PhotoLabelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
