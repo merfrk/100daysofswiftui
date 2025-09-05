926e24adf9590042ae6abcee0bb18f54a499b172
merfrk <o.farukartan@hotmail.com>
2025-09-05 15:11:23 +0300

    feat: add TabViews

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -4,18 +4,33 @@
 //
 //  Created by Omer on 5.09.2025.
 //
-
+import UserNotifications
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView{
+            ProspectsView(filter: .none)
+                .tabItem{
+                    Label("Everyone", systemImage: "person.3")
+                }
+                
+            ProspectsView(filter: .contacted)
+                .tabItem{
+                    Label("Contacted", systemImage: "checkmark.circle")
+                }
+            
+            ProspectsView(filter: .uncontacted)
+                .tabItem{
+                    Label("Uncontacted", systemImage: "questionmark.diamond")
+                }
+            
+            MeView()
+                .tabItem{
+                    Label("Me", systemImage: "person.crop.square")
+                }
         }
-        .padding()
     }
 }
 
---
+++ HotProspects/HotProspects/MeView.swift
@@ -0,0 +1,18 @@
+//
+//  MeView.swift
+//  HotProspects
+//
+//  Created by Omer on 5.09.2025.
+//
+
+import SwiftUI
+
+struct MeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeView()
+}
