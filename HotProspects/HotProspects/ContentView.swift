783412d92f1076e56384228f6ccf5695802db45f
merfrk <o.farukartan@hotmail.com>
2025-09-05 12:14:02 +0300

    feat: create HotProspects

---
+++ HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Omer on 5.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
