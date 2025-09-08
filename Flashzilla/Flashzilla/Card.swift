48ade25f4b3bd44f3be3f00545c7d2b542e43aea
merfrk <o.farukartan@hotmail.com>
2025-09-08 14:40:46 +0300

    feat: add CardView

---
+++ Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,14 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Omer on 8.09.2025.
+//
+
+import Foundation
+struct Card {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -7,15 +7,73 @@
 
 import SwiftUI
 
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(y: offset * 10)
+    }
+}
+
 struct ContentView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
+    @State private var cards = Array<Card>(repeating: .example, count: 10)
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @Environment(\.scenePhase) var scenePhase
+    @State private var isActive = true
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+                VStack {
+                    Text("Time: \(timeRemaining)")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 5)
+                        .background(.black.opacity(0.75))
+                        .clipShape(.capsule)
+                    ZStack {
+                        ForEach(0..<cards.count, id: \.self) { index in
+                            CardView(card: cards[index]){
+                                withAnimation {
+                                           removeCard(at: index)
+                                       }
+                            }
+                                .stacked(at: index, in: cards.count)
+                        }
+                    }
+                }
+            if accessibilityDifferentiateWithoutColor {
+                VStack {
+                    Spacer()
+
+                    HStack {
+                        Image(systemName: "xmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(.circle)
+                        Spacer()
+                        Image(systemName: "checkmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(.circle)
+                    }
+                    .foregroundStyle(.white)
+                    .font(.largeTitle)
+                    .padding()
+                }
+            }
+            }
+        .onReceive(timer) { time in
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
         }
-        .padding()
+    }
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
     }
 }
 
