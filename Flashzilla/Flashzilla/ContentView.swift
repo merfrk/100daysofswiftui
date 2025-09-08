827b3dcced14b234ab2705f154a96cd8302570ed
merfrk <o.farukartan@hotmail.com>
2025-09-08 14:55:17 +0300

    feat: add a Timer

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -26,29 +26,38 @@ struct ContentView: View {
             Image(.background)
                 .resizable()
                 .ignoresSafeArea()
-                VStack {
-                    Text("Time: \(timeRemaining)")
-                        .font(.largeTitle)
-                        .foregroundStyle(.white)
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 5)
-                        .background(.black.opacity(0.75))
-                        .clipShape(.capsule)
-                    ZStack {
-                        ForEach(0..<cards.count, id: \.self) { index in
-                            CardView(card: cards[index]){
-                                withAnimation {
-                                           removeCard(at: index)
-                                       }
+            VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index]){
+                            withAnimation {
+                                removeCard(at: index)
                             }
-                                .stacked(at: index, in: cards.count)
                         }
+                        .stacked(at: index, in: cards.count)
                     }
                 }
+                .allowsHitTesting(timeRemaining > 0)
+                
+                if cards.isEmpty {
+                    Button("Start Again", action: resetCards)
+                        .padding()
+                        .background(.white)
+                        .foregroundStyle(.black)
+                        .clipShape(.capsule)
+                }
+            }
             if accessibilityDifferentiateWithoutColor {
                 VStack {
                     Spacer()
-
+                    
                     HStack {
                         Image(systemName: "xmark.circle")
                             .padding()
@@ -65,15 +74,33 @@ struct ContentView: View {
                     .padding()
                 }
             }
-            }
+        }
         .onReceive(timer) { time in
+            guard isActive else { return }
             if timeRemaining > 0 {
                 timeRemaining -= 1
+            }
+        }
+        .onChange(of: scenePhase) {
+            if scenePhase == .active {
+                if !cards.isEmpty{
+                    isActive = true
+                }
+            } else {
+                isActive = false
             }
         }
     }
     func removeCard(at index: Int) {
         cards.remove(at: index)
+        if cards.isEmpty {
+            isActive = false
+        }
+    }
+    func resetCards() {
+        cards = Array<Card>(repeating: .example, count: 10)
+        timeRemaining = 100
+        isActive = true
     }
 }
 
