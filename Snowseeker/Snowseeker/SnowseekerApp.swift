79f259c538063341fdc98185769b3a9146a6f27e
merfrk <o.farukartan@hotmail.com>
2025-09-17 22:33:13 +0300

    feat: create snowseeker project

---
+++ Snowseeker/Snowseeker/SnowseekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowseekerApp.swift
+//  Snowseeker
+//
+//  Created by Omer on 17.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct SnowseekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
