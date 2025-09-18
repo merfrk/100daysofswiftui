fa214d027765be09fb7aeeb64b6566af9c064475
merfrk <o.farukartan@hotmail.com>
2025-09-18 10:45:09 +0300

    feat: create views

---
+++ Snowseeker/Snowseeker/WelcomeView.swift
@@ -0,0 +1,24 @@
+//
+//  WelcomeView.swift
+//  Snowseeker
+//
+//  Created by Omer on 17.09.2025.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        VStack {
+                    Text("Welcome to SnowSeeker!")
+                        .font(.largeTitle)
+
+                    Text("Please select a resort from the left-hand menu; swipe from the left edge to show it.")
+                        .foregroundStyle(.secondary)
+                }
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
